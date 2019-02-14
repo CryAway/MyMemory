d810bee6a4e83aa1cfe63642066280d6fa3f4035
ienw <ienw@metropolia.fi>
2019-02-14 12:23:01 +0200

    LiLy

--- app/src/main/java/com/example/mymemory/MainActivity.java
+++ app/src/main/java/com/example/mymemory/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Hi I'm Nhannn
+        //Hello Lily is here.
     }
 }
