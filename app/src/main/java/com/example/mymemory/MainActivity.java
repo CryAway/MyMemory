4fbfe900a186076543840470de578d91f3729075
Your Name <maithieunhan@gmail.com>
2019-02-14 12:25:13 +0200

    Vy

--- app/src/main/java/com/example/mymemory/MainActivity.java
+++ app/src/main/java/com/example/mymemory/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Hi I'm Nhannn
         //Hello Lily is here.
+        //Vy
     }
 }
