a6662415228cc5dbd6d7accd050cee47dc3bb537
Your Name <maithieunhan@gmail.com>
2019-03-05 12:23:09 +0200

    Score list first run

--- app/src/main/java/com/example/mymemory/ScoreBoard.java
+++ app/src/main/java/com/example/mymemory/ScoreBoard.java
@@ -1,5 +1,6 @@
 package com.example.mymemory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,28 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ScoreBoard extends AppCompatActivity {
+    private ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
+        lv = findViewById(R.id.ScoreBoard);
+        PlayerScoreList.getInstance().add();
+        lv.setAdapter(new ArrayAdapter<PlayerScore>(
+                this,  //context (activity instance)
+                android.R.layout.simple_list_item_1, //XML item layout
+                PlayerScoreList.getInstance().getPlayerScoreList()) //array of data
+        );
 
+        /*lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent nextActivity = new Intent();
+                nextActivity.putExtra("players", i);
+                startActivity(nextActivity);
+
+            }
+        });*/
     }
 
 
